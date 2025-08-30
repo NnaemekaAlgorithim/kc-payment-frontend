0f8e89e23667f192aeb25f7d3b8c1de983297a09
NnaemekaAlgorithim <nnaemekanwodo625@gmail.com>
2025-08-30 09:28:19 +0100

    fixing the env url

--- dist/assets/wallet-C2tHAdoW.js
+++ dist/assets/wallet-C2tHAdoW.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-3nGxJqN5.js";/**
+import{r as s}from"./index-DA7ejbJb.js";/**
  * @license lucide-react v0.542.0 - ISC
  *
  * This source code is licensed under the ISC license.
